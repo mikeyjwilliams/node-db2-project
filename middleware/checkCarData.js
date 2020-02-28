dc054ac4ec293be68ed985e8ec546ba7167d4c5b
Michael Williams <manicdesigner@gmail.com>
2020-02-28 10:04:58 -0500

    comments marked

--- middleware/checkCarData.js
+++ middleware/checkCarData.js
@@ -3,11 +3,11 @@ module.exports = carData => {
     const { vin, make, model, mileage } = req.body;
 
     if (!vin) {
-      //? check for no vin
+      //! check for no vin
       return res.status(400).json({ message: 'car vin is required' });
     }
     if (vin) {
-      //? check for if vin doesnt have 17 nums or characters.
+      //! check for if vin does not have 17 nums or characters.
       if (vin.length !== 17) {
         return res
           .status(400)
@@ -15,15 +15,15 @@ module.exports = carData => {
       }
     }
     if (!make) {
-      // check no make
+      //! check no make
       return res.status(400).json({ message: 'car make is required' });
     }
     if (!model) {
-      // check for no model
+      //! check for no model
       return res.status(400).json({ message: 'car model is required' });
     }
     if (!mileage) {
-      // check for no mileage
+      //! check for no mileage
       return res.status(400).json({ message: 'car milage is required' });
     }
     next();
