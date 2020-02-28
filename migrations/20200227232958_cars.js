98bbe404149c3686cdbafed5e3cd492d3ec6ee23
Michael Williams <manicdesigner@gmail.com>
2020-02-27 23:43:56 -0500

    create cars migration

---
+++ migrations/20200227232958_cars.js
@@ -0,0 +1,21 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable('cars', tbl => {
+    tbl.increment('id');
+    tbl
+      .string('vin', 17)
+      .unique()
+      .notNullable();
+    tbl.string('make', 35).notNullable();
+    tbl.string('model', 75).notNullable();
+    tbl
+      .integer('mileage')
+      .unsigned()
+      .notNullable();
+    tbl.text('transmissionType').defaultTo('n/a');
+    tbl.text('title').defaultTo('n/a');
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('cars');
+};
