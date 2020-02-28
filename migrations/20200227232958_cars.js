3dba040aca40a29f7cdc10e126df18b620568361
Michael Williams <manicdesigner@gmail.com>
2020-02-27 23:45:16 -0500

    fix increments id

--- migrations/20200227232958_cars.js
+++ migrations/20200227232958_cars.js
@@ -1,6 +1,6 @@
 exports.up = async function(knex) {
   await knex.schema.createTable('cars', tbl => {
-    tbl.increment('id');
+    tbl.increments('id');
     tbl
       .string('vin', 17)
       .unique()
