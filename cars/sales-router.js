f7a55cf01398109220dc22913ab218874893c111
Michael Williams <manicdesigner@gmail.com>
2020-02-29 01:27:16 -0500

    start salesRouter

---
+++ cars/sales-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const db = require('../data/config');
+
+router.post('/', async (req, res, next) => {});
+
+module.exports = router;
