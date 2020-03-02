bc5943e6f5a69eb8241c14871c8840a65600339e
Mikey Williams <manicdesigner@gmail.com>
2020-03-02 16:20:55 -0500

    fix last seed insert

--- seeds/2_sales.js
+++ seeds/2_sales.js
@@ -1,27 +1,28 @@
 exports.seed = async function(knex) {
-  await knex('sales').insert([
-    {
-      sellers_name: 'mikey williams',
-      buyers_name: 'jeff ristow',
-      sales_date: '2020-01-29',
-      sold: 1,
-      price: 2500.45,
-      cars_id: 1,
-    },
-    {
-      sellers_name: 'jeff ristow',
-      buyers_name: 'goku dragonball',
-      sales_date: '2020-02-02',
-      sold: 1,
-      price: 2323.23,
-      cars_id: 1,
-    },
-    {
-      sellers_name: 'gina frost',
-      buyers_name: 'n/a',
-      price: 0,
-      sold: 0,
-      cars_id: 2,
-    },
-  ]);
+	await knex('sales').insert([
+		{
+			sellers_name: 'mikey williams',
+			buyers_name: 'jeff ristow',
+			sales_date: '2020-01-29',
+			sold: 1,
+			price: 2500.45,
+			cars_id: 1
+		},
+		{
+			sellers_name: 'jeff ristow',
+			buyers_name: 'goku dragonball',
+			sales_date: '2020-02-02',
+			sold: 1,
+			price: 2323.23,
+			cars_id: 1
+		},
+		{
+			sellers_name: 'gina frost',
+			buyers_name: 'james harmon',
+			sales_date: '2020-03-01',
+			price: 35000,
+			sold: 1,
+			cars_id: 2
+		}
+	]);
 };
